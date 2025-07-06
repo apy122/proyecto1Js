2421d410ce46b2213a79174e4a39a4646259fcd3
apy122 <alvaro.perez532@comunidadunir.net>
2025-07-06 16:32:04 +0200

    modify: variared changed version 5.5

--- public/js/contacto.js
+++ public/js/contacto.js
@@ -1,5 +1,82 @@
-import { contacto } from "./contacto/contactoFunctions.js";
 
-document.addEventListener("DOMContentLoaded", function() {
-   contacto();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const validation = new JustValidate(".main__contact");
+  let isValidated=false;
+  validation
+    .addField("#nameform", [
+      {
+        rule: "required",
+        errorMessage: "El nombre es obligatorio"
+      },
+      {
+        rule: "minLength",
+        value: 3,
+        errorMessage: "El nombre es muy corto"
+      },
+      {
+        rule: "maxLength",
+        value: 25,
+        errorMessage: "El nombre es demasiado largo"
+      }
+    ])
+    .addField("#surname", [
+      {
+        rule: "required",
+        errorMessage: "El apellido es obligatorio"
+      },
+      {
+        rule: "minLength",
+        value: 3,
+        errorMessage: "El apellido es muy corto"
+      },
+      {
+        rule: "maxLength",
+        value: 15,
+        errorMessage: "El apellido es demasiado largo"
+      }
+    ])
+    .addField("#emailform", [
+      {
+        rule: "required",
+        errorMessage: "El email es obligatorio"
+      },
+      {
+        rule: "email",
+        errorMessage: "formato no valido"
+      }
+    ])
+    .addField("#gender", [
+      {
+        rule: "required",
+        errorMessage: "El genero es obligatorio"
+      }
+    ])
+    .addField("#birth", [
+      {
+        rule: "required",
+        errorMessage: "El genero es obligatorio"
+      }
+    ])
+     .addField("#years", [
+      {
+        rule: "required",
+        errorMessage: "la edad es obligatorio"
+      }
+    ])
+    .onSuccess(()=>{
+      isValidated= true;
+     const user= {
+      nombre: document.querySelector("#nameform").value,
+      apellido: document.querySelector("#surname").value,
+      email: document.querySelector("#emailform").value,
+      genero: document.querySelector("#gender").value,
+      nacimiento: document.querySelector("#birth").value,
+      edad: document.querySelector("#years").value
+     };
+     let userJson=JSON.stringify(user);
+     console.log(userJson);
+    });
+
+   
 });
--- public/js/contacto/contactoFunctions.js
+++
@@ -1,3 +0,0 @@
-export function contacto(){
-    return null;
-}
